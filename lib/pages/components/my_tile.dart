c73e0c17a6a97d052ed575cff2e8454d40fb01fd
NataSDep <kokosilalahi76@gmail.com>
2023-12-02 15:39:23 +0700

    First-commit-nata

--- lib/pages/components/my_tile.dart
+++ lib/pages/components/my_tile.dart
@@ -17,64 +17,65 @@ class MyTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child: Container(
-        height: 360,
-        decoration: BoxDecoration(
-          border: Border.all(width: 1),
-          color: Colors.grey[200],
-          borderRadius: BorderRadius.circular(30),
-        ),
-        margin: const EdgeInsets.only(
-          bottom: 10,
-          top: 10,
-        ),
-        //padding: EdgeInsets.all(25),
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                calon.nomorUrut,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25,
+      child: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(width: 1),
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(30),
+          ),
+          margin: const EdgeInsets.only(
+            bottom: 10,
+            top: 10,
+          ),
+          //padding: EdgeInsets.all(25),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  calon.nomorUrut,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              Image.asset(
-                calon.imagePath,
-              ),
-              const SizedBox(height: 10),
-              SizedBox(
-                child: Column(
-                  children: [
-                    const SizedBox(height: 10),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        calon.namaCapres,
-                        style: const TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.w500,
+                const SizedBox(height: 10),
+                Image.asset(
+                  calon.imagePath,
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 10),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          calon.namaCapres,
+                          style: const TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 10),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: Text(
-                        calon.namaCawapres,
-                        style: const TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.w500,
+                      const SizedBox(height: 10),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          calon.namaCawapres,
+                          style: const TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
