31e9f90aa4837e9746037eb71683b51dd4f0099d
NataSDep <kokosilalahi76@gmail.com>
2024-03-31 21:28:39 +0700

    Commit 14

--- lib/pages/news/explore.dart
+++ lib/pages/news/explore.dart
@@ -10,6 +10,7 @@ import 'package:skripsilocal/pages/news/FilterExplore.dart';
 import 'package:skripsilocal/pages/news/afterSearchExplore.dart';
 import 'package:flutter/services.dart';
 
+import '../../Utils/CategoryUtils.dart';
 import '../../repository/authentication_repository/authentication_repository.dart';
 import 'NewsDetail.dart';
 
@@ -26,6 +27,9 @@ class _ExplorePageState extends State<ExplorePage> {
   List<String> filters = ['Terbaru', 'Terlama'];
   final controller = Get.put(NewsController());
   late Future<List<NewsModel>> _futureNewsList;
+  List<String> userCategoryOver = [];
+  final listCategoryController = Get.put(CategoryListParser());
+
 
   @override
   void initState() {
@@ -181,6 +185,47 @@ class _ExplorePageState extends State<ExplorePage> {
                                                   await Future.delayed(const Duration(milliseconds: 100));
                                                   userController.updateUserScoreCategory(snapshot.data![index].category);
                                                 }
+
+                                                // Check Score over limit
+                                                userCategoryOver = [];
+                                                await Future.delayed(const Duration(milliseconds: 100));
+                                                String flagCategoryOver = userController.getFlagOverScore();
+                                                await Future.delayed(const Duration(milliseconds: 100));
+                                                List<int> indexOverScore = [];
+                                                indexOverScore = userController.getListIndexOverScoreCategory();
+
+                                                if(flagCategoryOver == "TRUE"){
+                                                  userCategoryOver = listCategoryController.parseScoreOverToList(indexOverScore);
+                                                }
+
+                                                // Data yg over limit dikirim dalam bentuk list string
+
+                                                // Ekspetasi dari FE :
+                                                // Kirim list kategori yang dipilih dan yang tidak dipilih
+
+                                                String persetujuan = "";
+                                                List<String> listKategoriYgMauDiUpdate = [];
+                                                List<String> listKategoriYgTidakUpdate = userCategoryOver;
+
+                                                // Kalau YES ==> Jadi Favorit, Score = 1000
+                                                // Kalau NO ==> Score kembali jadi 0.
+
+                                                if(listKategoriYgMauDiUpdate.length !=0){
+                                                  await Future.delayed(const Duration(milliseconds: 50));
+                                                  for(int i=0; i<listKategoriYgMauDiUpdate.length; i++){
+                                                    await Future.delayed(const Duration(milliseconds: 100));
+                                                    userController.updateUserScoreOverLimitCategory(listKategoriYgMauDiUpdate[i].toString(), 1000);
+                                                  }
+                                                }
+
+                                                if(listKategoriYgTidakUpdate.length !=0){
+                                                  await Future.delayed(const Duration(milliseconds: 50));
+                                                  for(int i=0; i<listKategoriYgTidakUpdate.length; i++){
+                                                    await Future.delayed(const Duration(milliseconds: 100));
+                                                    userController.updateUserScoreOverLimitCategory(listKategoriYgTidakUpdate[i].toString(), 0);
+                                                  }
+                                                }
+
                                                 Get.to(() => NewsDetail(
                                                   id: snapshot.data![index].id.toString(),
                                                   title: snapshot.data![index].title,
